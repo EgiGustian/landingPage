85fa97670ebc17fd78e574c973595ca13a3e1cc0
Egi Gustian Maulana <119639520+EgiGustian@users.noreply.github.com>
2025-11-03 20:46:43 +0700

    Landing Page

---
+++ script.js
@@ -0,0 +1,19 @@
+// Toggle menu untuk mobile
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Smooth scroll
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+        navLinks.classList.remove('active'); // Tutup menu setelah klik
+    });
+});
